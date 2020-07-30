35eef1a280c771b95c732474dff594b38f56281b
renfrid <renfrid@2014>
2020-07-30 13:34:30 +0300

    Minor change on base model

--- src/utils/models.py
+++ src/utils/models.py
@@ -4,7 +4,7 @@ from authtools.models import User
 # Create your models here.
 class BaseModel(models.Model):
     created_on      = models.DateTimeField(auto_now_add=True, editable=False)
-    created_by      = models.ForeignKey(User, related_name='course_created_by', on_delete=models.PROTECT)
+    created_by      = models.ForeignKey(User, related_name='created_by', on_delete=models.PROTECT)
 
 class Meta:
     abstract=True # Set this model as Abstract
