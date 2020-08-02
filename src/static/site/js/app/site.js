4907b01002fe85a379864764773981a803473f39
mchizani <ericbeda@gmail.com>
2020-08-03 00:19:23 +0300

    added animation, and css styling  to lesson scroll

--- src/static/site/js/app/site.js
+++ src/static/site/js/app/site.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 
     $(".section_item").click(function(){
+
         var lesson_id   = $(this).attr('wrp_id');
         $('.lesson_wrp').hide();
         $('#'+lesson_id).slideDown();
@@ -13,4 +14,15 @@ $(document).ready(function(){
         $("#lesson_content_vid source").attr('src', video_src);
         $("#lesson_content_vid").load();
     });
+
+    $(".section_item, .lesson_item").click(function(){
+
+        var topPos     = $("#scrollable").scrollTop();
+        var divPos     = $(this).offset().top - 125;
+        $("#scrollable").animate({ scrollTop: divPos+topPos });
+
+        $(".section_item, .lesson_item").removeClass('active_item');
+        $(this).addClass('active_item');
+    });
+
   });
